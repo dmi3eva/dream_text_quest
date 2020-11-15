94af9dbff9e51b4632b43da7f07c7eca3a9adf64
dmi3eva <forkatdmi@gmail.com>
2020-11-15 19:58:11 +0300

    ADD: configuration

--- run_bot.py
+++ run_bot.py
@@ -1,6 +1,5 @@
 import telebot
-
-API_TOKEN = 'YOUR_TOKEN_SHOULD_BE_HERE'  # TODO: Вставьте сюда API_TOKEN вашего бота
+from config import API_TOKEN
 
 bot = telebot.TeleBot(API_TOKEN)
 controller = {}
@@ -21,8 +20,8 @@ def start_message(message):
                      [4] Кнопка "Известная личность"
                      [5] Кнопка "Ваш преподаватель по проектному программированию"
                      """)
-    user_id = message.from_user.id
-    controller[user_id] = 'start'
+    x = message.from_user.id
+    controller[x] = 'start'
 
 
 @bot.message_handler(content_types=['text'])
