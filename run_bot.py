a34c149273b9fad2e8a0f60a84ab52027cfa4570
dmi3eva <forkatdmi@gmail.com>
2020-11-15 20:10:30 +0300

    Initial commit

--- run_bot.py
+++ run_bot.py
@@ -21,8 +21,8 @@ def start_message(message):
                      [4] Кнопка "Известная личность"
                      [5] Кнопка "Ваш преподаватель по проектному программированию"
                      """)
-    x = message.from_user.id
-    controller[x] = 'start'
+    user_id = message.from_user.id
+    controller[user_id] = 'start'
 
 
 @bot.message_handler(content_types=['text'])
